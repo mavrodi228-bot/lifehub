44dd4028ae263b27c8fbf56d73c4c67128af1425
Peter <petrporokin@gmail.com>
2026-06-19 11:01:05 +0400

    changes

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 window.LIFEHUB_CONFIG = {
-  supabaseUrl: 'https://avikkeimjvojhqpavheh.supabase.co',
-  supabaseAnonKey: 'sb_publishable_iZbcmW2-jf0n0nZcqhUmgw_O7_4Hkgp',
-  vapidPublicKey: '',
-  appName: 'LifeHub',
+  supabaseUrl: "https://avikkeimjvojhqpavheh.supabase.co",
+  supabaseAnonKey: "sb_publishable_iZbcmW2-jf0n0nZcqhUmgw_O7_4Hkgp",
+  vapidPublicKey:
+    "BNCw41yTf5LXnpOY0kWLiwyvsEOx6eArXTxZ6lH1H873u0_lY15roiYHjPDGdeYamxrdy66QBceUcrchNUTDG50",
+  appName: "LifeHub",
 };
